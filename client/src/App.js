55e0786b88d08e0e091fed6ffe0cc22f53861b1b
shanthisravana <shanthi.sravana@gmail.com>
2020-10-04 01:36:05 -0700

    Working Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> My React App </h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
